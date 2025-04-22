9ff8c3aea80139ab751b5143f5f15a9fc0be6f26
Ivan Kabanovskyi <kabanovskiy.ivan@gmail.com>
2025-04-22 15:28:35 +0300

    fixAndroid

--- src/js/copCompass.js
+++ src/js/copCompass.js
@@ -80,7 +80,7 @@ const compassCircle = document.querySelector(".compass-circle");
     }
     function getCompassHeading(e) {
       if (e.absolute === true && e.alpha != null) {
-        let alpha = (e.alpha + 90) % 360;
+        let alpha = (e.alpha + 120) % 360;
         if (typeof window.orientation !== "undefined") {
           alpha = alpha - window.orientation;
         }
